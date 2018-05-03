596a0c38e6d95a62e33fb3d8ebb287dbce10bbff
Alecktos <alexander.persson@fortnox.se>
2018-05-03 11:59:16 +0200

    new stuff

--- java-exercises/workshop 1/src/WA4Regression.java
+++ java-exercises/workshop 1/src/WA4Regression.java
@@ -1,12 +1,36 @@
+import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class WA4Regression {
+
+	public Instances loadDataset(String fileName) {
+		Instances trainData = null;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			ArffLoader.ArffReader arff = new ArffLoader.ArffReader(reader);
+			trainData = arff.getData();
+			System.out.println("===== Loaded dataset: " + fileName + " =====");
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("Problem found when reading: " + fileName);
+		}
+		return trainData;
+	}
+
+	private void iterate() {
+
+	}
 
 	public static void main (String[] args) {
 		//Classify the Iris dataset in Weka using the algorithm k-Nearest Neighbor (lazy/IBk), Decision Trees (trees/J48) and Naïve Bayes (bayes/NaiveBayes)
 
-		WA32NumericalAttributesClassification learner = new WA32NumericalAttributesClassification();
-		Instances trainData = learner.loadDataset("/home/alexander/Projects/Applied-Machine-Learning-Course/Iris/iris.arff");
+		WA4Regression learner = new WA4Regression();
+		Instances trainData = learner.loadDataset("/home/alexander/Projects/Applied-Machine-Learning-Course/GPUbenchmark/GPUbenchmark.arff");
 
 		//Naive Bayes
 		//NaiveBayes naiveBayes = new NaiveBayes();
